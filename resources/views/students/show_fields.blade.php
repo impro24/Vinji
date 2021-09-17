3f22b0026c23c7d366e13358b0b00ba3ee4d72c7
unknown <arvinjayudtohan@gmail.com>
2021-09-18 01:03:59 +0800

    Initial Commit

---
+++ resources/views/students/show_fields.blade.php
@@ -0,0 +1,48 @@
+<!-- Firstname Field -->
+<div class="col-sm-12">
+    {!! Form::label('Firstname', 'Firstname:') !!}
+    <p>{{ $students->Firstname }}</p>
+</div>
+
+<!-- Middlename Field -->
+<div class="col-sm-12">
+    {!! Form::label('Middlename', 'Middlename:') !!}
+    <p>{{ $students->Middlename }}</p>
+</div>
+
+<!-- Lastname Field -->
+<div class="col-sm-12">
+    {!! Form::label('Lastname', 'Lastname:') !!}
+    <p>{{ $students->Lastname }}</p>
+</div>
+
+<!-- Birthdate Field -->
+<div class="col-sm-12">
+    {!! Form::label('Birthdate', 'Birthdate:') !!}
+    <p>{{ $students->Birthdate }}</p>
+</div>
+
+<!-- Gender Field -->
+<div class="col-sm-12">
+    {!! Form::label('Gender', 'Gender:') !!}
+    <p>{{ $students->Gender }}</p>
+</div>
+
+<!-- Address Field -->
+<div class="col-sm-12">
+    {!! Form::label('Address', 'Address:') !!}
+    <p>{{ $students->Address }}</p>
+</div>
+
+<!-- Citizenshp Field -->
+<div class="col-sm-12">
+    {!! Form::label('Citizenshp', 'Citizenshp:') !!}
+    <p>{{ $students->Citizenshp }}</p>
+</div>
+
+<!-- Religion Field -->
+<div class="col-sm-12">
+    {!! Form::label('Religion', 'Religion:') !!}
+    <p>{{ $students->Religion }}</p>
+</div>
+
